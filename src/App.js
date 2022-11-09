51a67e0850b98037ee7883641cf513acdd162e87
Rodrigo Garoz <35437197+GDawg4@users.noreply.github.com>
2022-11-09 15:19:17 -0600

    Commit importante

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-            Buenas noches 123
+            Probando esta funcionalidad que me parece interesante
         </p>
       </div>
     );
