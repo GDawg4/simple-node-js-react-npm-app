8d6627979b2e9ac42e0465beaf3b85f83da89084
Rodrigo Garoz <35437197+GDawg4@users.noreply.github.com>
2022-11-16 18:19:44 -0600

    Commit importante

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-            Prueba en vivo y en directo
+            Prueba en vivo y en directo para la grabación y la posteridad
         </p>
       </div>
     );
